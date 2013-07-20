80910ec8a16818d00bde62bb90d00bba74487eee
Przemek <Przemek@KompStacjon>
2013-07-20 12:44:57 +0100

    Navigation panel is funvctional: shift the plot left, right, zooms in and out.
    TODO: add a field to the navi panel to allow the users set the value of the shift, as the ranges of the axes may be of various ranks.
    
    TODO: move the code generating the creation panel to a different class, later on a factory of naviPanels.

--- src/gubas/components/GubasChart.java
+++ src/gubas/components/GubasChart.java
@@ -8,26 +8,35 @@ import gubas.icons.Icons;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.*;
 import org.jfree.chart.ChartPanel;
-import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.ValueAxis;
 import org.jfree.chart.plot.Plot;
+import org.jfree.data.Range;
 
 /**
  *
  * @author Przemysław
  */
-public class GubasChart extends JPanel{
+public class GubasChart extends JPanel implements ActionListener{
     
     
     Plot myPlot = null;
     ChartPanel chart = null;
+    double shiftValue = 1; //this value will be editable for the users ina textfield in the UI.
 
+
+    
+    protected final String ZOOM_IN_COMMAND = "zoomin";
+    protected final String ZOOM_OUT_COMMAND = "zoomout";
+    protected final String MOVE_LEFT_COMMAND = "moveLeft";
+    protected final String MOVE_RIGHT_COMMAND = "moveRight";
+    
     public ChartPanel getChart() {
         return chart;
     }
@@ -36,9 +45,16 @@ public class GubasChart extends JPanel{
         this.chart = chart;
         this.setLayout(new BorderLayout());
         this.add(chart, BorderLayout.CENTER);
-        this.add(getNavigation(), BorderLayout.SOUTH);
+        //This should be done only for charts that display in XY form
+        if(chart.getChart().getXYPlot()!=null){
+            this.add(getNavigation(), BorderLayout.SOUTH);
+        }
+        
     }
  
+    protected double calculateRangeChanges(Range r, double coeff){
+        return  coeff*(r.getUpperBound()-r.getLowerBound());
+    }
     
     protected JPanel getNavigation(){
         JPanel naviPan = new JPanel(new BorderLayout());
@@ -57,11 +73,16 @@ public class GubasChart extends JPanel{
         moveLeft.setPreferredSize(new Dimension(40,40));
         moveRight.setPreferredSize(new Dimension(40,40));
         zoomIn.setPreferredSize(new Dimension(40,40));
+        zoomIn.setActionCommand(ZOOM_IN_COMMAND);
         zoomOut.setPreferredSize(new Dimension(40,40));
+        zoomOut.setActionCommand(ZOOM_OUT_COMMAND);
+        moveLeft.setActionCommand(MOVE_LEFT_COMMAND);
+        moveRight.setActionCommand(MOVE_RIGHT_COMMAND);
         JButton[] buttons = new JButton[]{moveLeft, zoomIn, zoomOut, moveRight};
         //add the buttons to the panel
         for(int i=0; i<buttons.length; i++){
             buttonsPanel.add(buttons[i]);
+            buttons[i].addActionListener(this);
         }
         buttonsPanel.setPreferredSize(new Dimension(50, 60));
         buttonsPanel.setBackground(Color.WHITE);
@@ -81,5 +102,29 @@ public class GubasChart extends JPanel{
             b = new JButton(alternativeString);
         }
         return b;
+    }
+    
+    protected void shiftDomainAxis(ValueAxis vax, int direction, double distance){
+        double d = direction*distance;
+        vax.setRange(vax.getRange().getLowerBound()+ d, vax.getRange().getUpperBound()+d);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        ChartPanel p = this.chart;
+       switch(e.getActionCommand()){
+           case ZOOM_IN_COMMAND :
+               p.zoomInDomain(p.getX()+ p.getWidth()/2, p.getY()+p.getHeight()/2);
+               break;
+           case ZOOM_OUT_COMMAND:
+               p.zoomOutDomain(p.getX()+ p.getWidth()/2, p.getY()+p.getHeight()/2);
+               break;
+           case MOVE_LEFT_COMMAND:
+               shiftDomainAxis(p.getChart().getXYPlot().getDomainAxis(), -1, shiftValue);
+               break;
+           case MOVE_RIGHT_COMMAND:
+              shiftDomainAxis(p.getChart().getXYPlot().getDomainAxis(), 1, shiftValue);
+               break;
+       }
     }
 }
